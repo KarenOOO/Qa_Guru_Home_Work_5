7bbc745ebde374a84a269beeeca1131a4b3c2e8e
Oganesyan Karen <kar99-99@bk.ru>
2025-05-31 22:45:29 +0300

    Added DragDropTests

---
+++ src/test/java/DragDropTests.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDropTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void moveElementTest() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldNotHave(text("B"));
+
+        actions().dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldNotHave(text("A"));
+    }
+}
