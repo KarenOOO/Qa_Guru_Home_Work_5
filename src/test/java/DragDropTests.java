543a92b71f563cc2480a725789c2d6e44ca3cf6f
Oganesyan Karen <kar99-99@bk.ru>
2025-06-01 07:53:42 +0300

    Added check

--- src/test/java/DragDropTests.java
+++ src/test/java/DragDropTests.java
@@ -17,12 +17,22 @@ public class DragDropTests {
     @Test
     void moveElementTest() {
         open("/drag_and_drop");
+
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
         $("#column-a").dragAndDropTo($("#column-b"));
         $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
         $("#column-b").shouldNotHave(text("B"));
+        $("#column-a").shouldNotHave(text("A"));
+
 
         actions().dragAndDrop($("#column-a"), $("#column-b")).perform();
         $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         $("#column-b").shouldNotHave(text("A"));
+        $("#column-a").shouldNotHave(text("B"));
+
     }
 }
